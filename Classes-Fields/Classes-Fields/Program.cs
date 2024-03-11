b29d9da49809cd20904c0af4481b03e73afc7c1a
Alex Evans <aevans@quiktrip.com>
2024-03-11 12:02:31 -0500

    added fields project

---
+++ Classes-Fields/Classes-Fields/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Fields
+{
+    class Program
+    {
+        public class Bio
+        {
+            public string name;
+            public int age;
+            public string hometown;
+
+            public Bio(string bioName, int bioAge, string bioHometown)
+            {
+                name = bioName; 
+                age = bioAge;
+                hometown = bioHometown;
+            }
+            
+            public void biography()
+            {
+                Console.WriteLine($"{name}: {age}: {hometown}");
+            }
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            Bio test1 = new Bio("Alex", 23, "Springfield");
+            test1.biography();
+        }
+    }
+
+       
+}
