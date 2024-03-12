452d96ee343225216c25e33be56b93b4dd891bce
Alex Evans <aevans@quiktrip.com>
2024-03-12 10:13:02 -0500

    added project

--- Shape-Calculator/Shape-Calculator/obj/Release/net8.0/Shape-Calculator.AssemblyInfo.cs
+++ Shape-Calculator/Shape-Calculator/obj/Release/net8.0/Shape-Calculator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shape-Calculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d508620d5b43895f7ba476af32f112f257623749")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shape-Calculator")]
